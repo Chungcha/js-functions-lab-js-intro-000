1e4840ac1756f6671344b5aac2b73f5fad7d8646
Charlie Chung <charlieschung@gmail.com>
2019-07-17 00:06:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown(holidays, days){
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`;
 }
